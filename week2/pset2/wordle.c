b4be6363ae05212887589fe16464fe4e4f8773f6
Marian Baba <marianbaba16@gmail.com>
2023-10-24 20:50:58 +0200

    complete implementation of the wordle50 game

--- week2/pset2/wordle.c
+++ week2/pset2/wordle.c
@@ -14,20 +14,70 @@
 #define RESET_TEXT          "\033[0m"
 
 void validate_input(int argc, const string argv[]);
+string get_word(string filename);
+string get_guess(int word_size);
+bool check_guess(string guess, string correct_word);
 
 int main(int argc, string argv[])
 {
+    // input validation
     validate_input(argc, argv);
-    srand(time(NULL));
+
+    // get cli param
     int size = atoi(argv[1]);
     int tries = size + 1;
     printf("\n"GREEN_TEXT"*** This is WORDLE50 ***"RESET_TEXT"\n\n");
     printf("-> You have %i tries to guess the %i-letter word I'm thinking of...\n\n", tries, size);
 
+    // choose file from where to read
     char filename[6];
     string extension = ".txt";
     snprintf(filename, sizeof(filename), "%d%s", size, extension);
+    
+    // choose the word
+    string correct_word = get_word(filename);
+    printf("Randomly selected word: %s\n", correct_word);
+    
+    // for each try available, let the user guess
+    int i;
+    for (i = 0; i < tries; i++)
+    {   
+        // get guess from user (a ${size}-letter word)
+        printf("Guess %i -> ", (i + 1));
+        string guess = get_guess(size);
 
+        // check guess has to: see if the guess is equal to the correct word, print the chars accondingly and return a boolean (has won or not)
+        bool won = check_guess(guess, correct_word);
+        if (won)
+        {
+            printf("\n\n"GREEN_BACKGROUND"Good guess! You got the word!"RESET_BACKGROUND"\n");
+            break;
+        }
+    }
+    if (i == tries)
+    {
+        printf("\n\n"RED_BACKGROUND"Sorry! You run out of tries!"RESET_BACKGROUND"\n");
+    }
+    return 0;
+}
+
+void validate_input(int argc, const string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./wordle wordsize\n");
+        exit(1);
+    }
+    int input = atoi(argv[1]);
+    if (!(input >= 5 && input <= 8))
+    {
+        printf("Error: wordsize must be either 5, 6, 7, or 8\n");
+        exit(1);
+    }
+}
+
+string get_word(string filename)
+{
     FILE *file;
     file = fopen(filename, "r");
     if (file == NULL)
@@ -49,24 +99,98 @@ int main(int argc, string argv[])
         word_count++;
     }
     fclose(file);
-    int random_index = rand() % 1000;
-    // Print the randomly selected word
-    printf("Randomly selected word: %s\n", words[random_index]);
 
-    return 0;
+    // choose a random index
+    srand(time(NULL));
+    int random_index = rand() % 1000;
+
+    // choose the word
+    string correct_word = words[random_index];
+    return correct_word;
 }
 
-void validate_input(int argc, const string argv[])
+string get_guess(int word_size)
 {
-    if (argc != 2)
+    string guess;
+    do
     {
-        printf("Usage: ./wordle wordsize\n");
-        exit(1);
-    }
-    int input = atoi(argv[1]);
-    if (!(input >= 5 && input <= 8))
+        guess = get_string("Input a %d-letter word: ", word_size);
+    } while (strlen(guess) != word_size);
+    return guess;
+}
+
+bool check_guess(string guess, string correct_word)
+{
+    bool res = true;
+    int len = strlen(guess);
+    char colors[len];
+
+    // Arrays to keep track of matched positions in both the guess and the correct word
+    bool matched_guess[len];
+    bool matched_word[len];
+
+    for (int i = 0; i < len; i++) 
     {
-        printf("Error: wordsize must be either 5, 6, 7, or 8\n");
-        exit(1);
+        matched_guess[i] = false;
+        matched_word[i] = false;
     }
+
+    // First pass: check for characters in the correct position (green)
+    for (int i = 0; i < len; i++) 
+    {
+        if (guess[i] == correct_word[i]) 
+        {
+            colors[i] = 'G';
+            matched_guess[i] = true;
+            matched_word[i] = true;
+        }
+    }
+
+    // Second pass: check for characters in the correct word but in the wrong position (yellow)
+    for (int i = 0; i < len; i++) 
+    {
+        if (!matched_guess[i]) 
+        { // Skip characters already matched in the first pass
+            for (int j = 0; j < len; j++) 
+            {
+                if (guess[i] == correct_word[j] && !matched_word[j]) 
+                {
+                    colors[i] = 'Y';
+                    matched_guess[i] = true;
+                    matched_word[j] = true;
+                    break; // Exit the inner loop
+                }
+            }
+        }
+    }
+
+    // Third pass: mark remaining unmatched characters as red
+    for (int i = 0; i < len; i++)
+    {
+        if (!matched_guess[i]) 
+        {
+            colors[i] = 'R';
+            res = false; // Set res to false if there are unmatched characters
+        }
+    }
+
+    // Print characters with appropriate colors
+    for (int i = 0; i < len; i++) 
+    {
+        if (colors[i] == 'G') 
+        {
+            printf(GREEN_TEXT"%c"RESET_TEXT, guess[i]);
+        } 
+        else if (colors[i] == 'Y') 
+        {
+            printf(YELLOW_TEXT"%c"RESET_TEXT, guess[i]);
+        } 
+        else if (colors[i] == 'R') 
+        {
+            printf(RED_TEXT"%c"RESET_TEXT, guess[i]);
+        }
+    }
+    printf("\n");
+
+    return res;
 }
